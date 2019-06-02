a2033bb591bb97eee70a3a1c05e2bbde64f23ad9
uvnode <51275687+uvnode@users.noreply.github.com>
2019-06-02 01:27:58 -0400

    Added synchronous methods

--- app/src/main/java/com/uvnode/typhoon/sources/HttpSource.java
+++ app/src/main/java/com/uvnode/typhoon/sources/HttpSource.java
@@ -2,14 +2,21 @@ package com.uvnode.typhoon.sources;
 
 import com.uvnode.typhoon.sources.api.ApiCallbacks;
 import com.uvnode.typhoon.sources.model.Genre;
+import com.uvnode.typhoon.sources.model.Listing;
 import com.uvnode.typhoon.sources.model.Ranking;
 import com.uvnode.typhoon.sources.model.Series;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import okhttp3.OkHttpClient;
 
 public abstract class HttpSource {
 
     private OkHttpClient okClient;
+
+    // Asynchronous
 
     public abstract void getRankings(ApiCallbacks apiConsumer);
 
@@ -22,6 +29,20 @@ public abstract class HttpSource {
     public abstract void getEpisodes(ApiCallbacks apiConsumer, Series series);
 
     public abstract void search(ApiCallbacks apiConsumer, String query, int page);
+
+    // Synchronous
+
+    public abstract ArrayList<Ranking> getRankings();
+
+    public abstract ArrayList<Genre> getGenres();
+
+    public abstract ArrayList<Series> getSeriesInRanking(Ranking ranking);
+
+    public abstract HashMap<String, Object> getSeriesInGenre(Genre genre, int page);
+
+    public abstract ArrayList<Listing> getEpisodes(Series series);
+
+    public abstract HashMap<String, Object> search(String query, int page);
 
     public void setOkClient(OkHttpClient okClient) {
         this.okClient = okClient;
