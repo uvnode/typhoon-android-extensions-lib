f9ef29fa6dd58ab267030899ece795c53303613c
uvnode <51275687+uvnode@users.noreply.github.com>
2020-06-17 18:33:34 -0400

    Added extension loading interface

---
+++ app/src/main/java/com/uvnode/typhoon/extensions/api/LoadExtension.java
@@ -0,0 +1,7 @@
+package com.uvnode.typhoon.extensions.api;
+
+import com.uvnode.typhoon.extensions.ExtensionHolder;
+
+public interface LoadExtension {
+    public ExtensionHolder getExtension();
+}
