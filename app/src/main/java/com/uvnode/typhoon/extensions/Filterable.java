b1fc5a2e5f2e5a61d1bd04695aec3bc279077a8d
uvnode <51275687+uvnode@users.noreply.github.com>
2020-06-26 17:28:07 -0400

    Changed response codes

--- app/src/main/java/com/uvnode/typhoon/extensions/Filterable.java
+++ app/src/main/java/com/uvnode/typhoon/extensions/Filterable.java
@@ -3,5 +3,5 @@ package com.uvnode.typhoon.extensions;
 import java.util.ArrayList;
 
 public interface Filterable {
-    
+
 }
--- app/src/main/java/com/uvnode/typhoon/extensions/api/ApiResponse.java
+++ app/src/main/java/com/uvnode/typhoon/extensions/api/ApiResponse.java
@@ -8,15 +8,15 @@ import java.lang.annotation.RetentionPolicy;
 public class ApiResponse {
 
     @Retention(RetentionPolicy.SOURCE)
-    @IntDef({UNCHANGED, NEW, UPDATE})
+    @IntDef({NONE, SOME})
     public @interface Result {}
 
-    public static final int UNCHANGED = 0;
-    public static final int NEW = 1;
-    public static final int UPDATE = 2;
+    public static final int NONE = 0;
+    public static final int SOME = 1;
 
     private Object payload;
     private int responseResult;
+    private boolean isSuccess;
 
     public ApiResponse(int result) {
         this.responseResult = result;
