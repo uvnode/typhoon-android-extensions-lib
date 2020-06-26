b0571c9e8dd49c8d3329cffe55f24e11d6ade400
uvnode <51275687+uvnode@users.noreply.github.com>
2020-06-26 19:38:15 -0400

    Added Configurable.java

---
+++ app/src/main/java/com/uvnode/typhoon/extensions/Configurable.java
@@ -0,0 +1,8 @@
+package com.uvnode.typhoon.extensions;
+
+import android.preference.PreferenceScreen;
+
+public interface Configurable {
+
+    public void setPreferences(PreferenceScreen preferences);
+}
