8b8af354b5611a945874384393d0ca108570b1a4
uvnode <51275687+uvnode@users.noreply.github.com>
2020-07-05 21:44:01 -0400

    Added filter types

--- app/src/main/java/com/uvnode/typhoon/extensions/model/Filter.java
+++ app/src/main/java/com/uvnode/typhoon/extensions/model/Filter.java
@@ -9,4 +9,38 @@ public class Filter<T> {
         this.name = name;
         this.state = state;
     }
+
+    class Text extends Filter<String> {
+        public Text(String name, String state) {
+            super(name, state);
+        }
+    }
+
+    class CheckBox extends Filter<Boolean> {
+        public CheckBox(String name, Boolean state) {
+            super(name, state);
+        }
+    }
+
+    class TriState extends Filter<Integer> {
+        static final int STATE_IGNORE = 0;
+        static final int STATE_INCLUDE = 1;
+        static final int STATE_EXCLUDE = 2;
+
+        public TriState(String name, Integer state) {
+            super(name, state);
+        }
+
+        public boolean isIgnored() {
+            return this.state == STATE_IGNORE;
+        }
+
+        public boolean isIncluded() {
+            return this.state == STATE_INCLUDE;
+        }
+
+        public boolean isExcluded() {
+            return this.state == STATE_EXCLUDE;
+        }
+    }
 }
